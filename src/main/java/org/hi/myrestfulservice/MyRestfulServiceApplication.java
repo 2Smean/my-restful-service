440dd1519e6a31441b8caca786560248a827c345
2Smean <136636643+2Smean@users.noreply.github.com>
2024-09-27 23:57:42 +0900

    feat : Annotation을 이용한 API추가
    Lombok 의 Data와 AllArgsConstructor 사용하여 Getmapping API 추가

--- src/main/java/org/hi/myrestfulservice/MyRestfulServiceApplication.java
+++ src/main/java/org/hi/myrestfulservice/MyRestfulServiceApplication.java
@@ -2,12 +2,20 @@ package org.hi.myrestfulservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class MyRestfulServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyRestfulServiceApplication.class, args);
+
+		// show bean list
+//		ApplicationContext ac
+//		String[] AllBeanNames = ac.getBeanDefinitionNames();
+//		for (String beanName : AllBeanNames) {
+//			System.out.println(beanName);
+//		}
 	}
 
 }
---
+++ src/main/java/org/hi/myrestfulservice/controller/HelloWorldController.java
@@ -0,0 +1,21 @@
+package org.hi.myrestfulservice.controller;
+
+import org.hi.myrestfulservice.bean.HelloWorldBean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    // GET
+    // URI - /hello-world
+    // @RequestMapping(method=RequestMethod.GET, path="/hello-world")
+    @GetMapping(path = "/hello-world")
+    public String helloworld() {
+        return "Hello World";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloworldBean() {
+        return new HelloWorldBean("Hello World");
+    }
+}

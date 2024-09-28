fdf24b8f0549a800b97d69243c038efaad9ec096
2Smean <136636643+2Smean@users.noreply.github.com>
2024-09-28 19:37:38 +0900

    feat : Annotation @PathVariable 사용법 익히기

--- src/main/java/org/hi/myrestfulservice/controller/HelloWorldController.java
+++ src/main/java/org/hi/myrestfulservice/controller/HelloWorldController.java
@@ -3,6 +3,7 @@ package org.hi.myrestfulservice.controller;
 import jakarta.servlet.Servlet;
 import org.hi.myrestfulservice.bean.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,5 +21,10 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloworldBean() {
         return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping(path = "/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloworldBeanPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 }

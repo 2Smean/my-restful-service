a44a63abce21dee4e29542aa6ef09c1c345f07c4
2Smean <136636643+2Smean@users.noreply.github.com>
2024-09-29 15:53:09 +0900

    feat : 사용자 등록을 위한 API 구현 - HTTP POST method
    - 200 번대 : 클라이언트 요청이 성공
        - 201 번대 : 생성에 대한 성공을 정확하게 구분해 줄 수 있는 코드
        - 204 번대 : 서버에서 응답 요청이 완료는 되었지만 특별한 반환 데이터가 없다는 코드
    - 400 번대 : 클라이언트 요청이 잘못 되었을 때
    - 500 번대 : 서버 측에서 exception 이나 오류 이슈가 발생했을 때

--- src/main/java/org/hi/myrestfulservice/controller/UserController.java
+++ src/main/java/org/hi/myrestfulservice/controller/UserController.java
@@ -2,9 +2,7 @@ package org.hi.myrestfulservice.controller;
 
 import org.hi.myrestfulservice.bean.User;
 import org.hi.myrestfulservice.dao.UserDaoService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,5 +22,10 @@ public class UserController {
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id) {
         return service.findOne(id);
+    }
+
+    @PostMapping("/users")
+    public void createUser(@RequestBody User user) {
+        User savedUser = service.save(user);
     }
 }

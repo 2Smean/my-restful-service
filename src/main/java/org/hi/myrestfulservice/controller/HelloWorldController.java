d43ea05a8f33242aeb98ee69f0ac96c807a93546
2Smean <136636643+2Smean@users.noreply.github.com>
2024-10-04 23:46:42 +0900

    feat : 다국어 처리를 위한 Internationalization 구현 방법
    internationalized Api 를 통한 다국어처리
    - messages.properties 를 각 언어별로 추가하여 각 Locale 별로 다른 인사말 나오게 함
    - @Bean 을 통해 localeResolver 등록하여 기본값은 영어로 세팅
    - 한글, 일본어 깨짐 현상으로 settings -> encoding -> resources 하위파일 add -> UTF-8 설정 을 통해 복원
    
    궁금한 점 : MyRestfulServiceApplication 에 Bean 을 추가했는데 주석처리하고 돌려도 Default 값은 영어로 잘 나옴. bean 주입이 어떤 역할을 하는지 명확히 모르겠다.

--- src/main/java/org/hi/myrestfulservice/controller/HelloWorldController.java
+++ src/main/java/org/hi/myrestfulservice/controller/HelloWorldController.java
@@ -2,12 +2,22 @@ package org.hi.myrestfulservice.controller;
 
 import jakarta.servlet.Servlet;
 import org.hi.myrestfulservice.bean.HelloWorldBean;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
     // GET
     // URI - /hello-world
     // @RequestMapping(method=RequestMethod.GET, path="/hello-world")
@@ -26,5 +36,11 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean/path-variable/{name}")
     public HelloWorldBean helloworldBeanPathVariable(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloworldInternationalized(
+            @RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+        return messageSource.getMessage("greeting.message", null, locale);
     }
 }

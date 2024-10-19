6c54bf9ea76d768c5c68a4315ab7b1ccfc66f918
2Smean <136636643+2Smean@users.noreply.github.com>
2024-10-19 23:27:36 +0900

    feat : Spring Data JPA를 이용한 초기 데이터 생성

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into users(id, join_date, name, password, ssn) values(90001, now(), 'User1', 'test111', '009093-3049582');
+insert into users(id, join_date, name, password, ssn) values(90002, now(), 'User2', 'test222', '990493-4958374');
+insert into users(id, join_date, name, password, ssn) values(90003, now(), 'User3', 'test333', '994857-4059384');

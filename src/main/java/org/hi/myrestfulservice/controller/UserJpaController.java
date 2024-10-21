d2ca7c30184ed1df5af37193f7dd5d1b8628a312
2Smean <136636643+2Smean@users.noreply.github.com>
2024-10-21 17:59:07 +0900

    feat : JPA를 이용한 사용자 추가와 삭제 - HTTP POST/DELETE method
    - 사용자 조회 API 구성 로직 풀어보기

--- src/main/java/org/hi/myrestfulservice/controller/UserJpaController.java
+++ src/main/java/org/hi/myrestfulservice/controller/UserJpaController.java
@@ -1,16 +1,16 @@
 package org.hi.myrestfulservice.controller;
 
+import jakarta.validation.Valid;
 import org.hi.myrestfulservice.bean.User;
 import org.hi.myrestfulservice.exception.UserNotFoundException;
 import org.hi.myrestfulservice.repository.UserRepository;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -36,6 +36,7 @@ public class UserJpaController {
     // /jpa/users/{id}
     @GetMapping("/users/{id}")
     public ResponseEntity retrieveUsersById(@PathVariable int id) {
+
         Optional<User> user = userRepository.findById(id);
 
         if (!user.isPresent()) {
@@ -48,5 +49,24 @@ public class UserJpaController {
         entityModel.add(linkTo.withRel("all-users")); // all-users -> http://localhost:8088/users
 
         return ResponseEntity.ok(entityModel);
+    }
+
+    // jpa/users/{id}
+    @DeleteMapping("users/{id}")
+    public void deleteUserById(@PathVariable int id) {
+        userRepository.deleteById(id);
+    }
+
+    // jpa/users
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
+        User savedUser = userRepository.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }

1a12dd8718c4538ae9870a373cb24b83a945fd5b
2Smean <136636643+2Smean@users.noreply.github.com>
2024-10-21 17:08:25 +0900

    feat : JPA를 이용한 개별 사용자 상세 조회 - HTTP Get method
    - JPA 의 findById 를 활용하여 해당 id 가 있다면 정보를 표시하고
    - 해당 id 가 존재하지 않는다면 UserNotFoundException 메세지 반환

--- src/main/java/org/hi/myrestfulservice/controller/UserJpaController.java
+++ src/main/java/org/hi/myrestfulservice/controller/UserJpaController.java
@@ -1,12 +1,21 @@
 package org.hi.myrestfulservice.controller;
 
 import org.hi.myrestfulservice.bean.User;
+import org.hi.myrestfulservice.exception.UserNotFoundException;
 import org.hi.myrestfulservice.repository.UserRepository;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/jpa")
@@ -22,5 +31,22 @@ public class UserJpaController {
     @GetMapping("/users")
     public List<User> retrieveAllUsers() {
         return userRepository.findAll();
+    }
+
+    // /jpa/users/{id}
+    @GetMapping("/users/{id}")
+    public ResponseEntity retrieveUsersById(@PathVariable int id) {
+        Optional<User> user = userRepository.findById(id);
+
+        if (!user.isPresent()) {
+            throw new UserNotFoundException("id-" + id);
+        }
+
+        EntityModel entityModel = EntityModel.of(user.get());
+
+        WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        entityModel.add(linkTo.withRel("all-users")); // all-users -> http://localhost:8088/users
+
+        return ResponseEntity.ok(entityModel);
     }
 }

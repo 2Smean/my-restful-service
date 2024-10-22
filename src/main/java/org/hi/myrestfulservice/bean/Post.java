27e13a92d9d3b70a87bcec0f4f28515c22518d3f
2Smean <136636643+2Smean@users.noreply.github.com>
2024-10-22 19:58:22 +0900

    feat : 게시물 관리를 위한 Post Entity 추가와 초기 데이터 생성

---
+++ src/main/java/org/hi/myrestfulservice/bean/Post.java
@@ -0,0 +1,24 @@
+package org.hi.myrestfulservice.bean;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //unique
+    private Integer id;
+
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonIgnore
+    private User user;
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,6 @@
 insert into users(id, join_date, name, password, ssn) values(90001, now(), 'User1', 'test111', '009093-3049582');
 insert into users(id, join_date, name, password, ssn) values(90002, now(), 'User2', 'test222', '990493-4958374');
 insert into users(id, join_date, name, password, ssn) values(90003, now(), 'User3', 'test333', '994857-4059384');
+
+insert into post(description, user_id) values ('My first post', 90001);
+insert into post(description, user_id) values ('My second post', 90001);
